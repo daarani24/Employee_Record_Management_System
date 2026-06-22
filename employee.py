d1bd7f54125ea23cac5e426a3c84e5a731c35417
daarani24 <daarani2007@gmail.com>
2026-06-22 15:49:32 +0530

    Initial commit

---
+++ employee.py
@@ -0,0 +1,9 @@
+class Employee:
+    def __init__ (self,employee_id,name,age,department,experience,salary):
+        self.emp_id=employee_id
+        self.name=name
+        self.age=age
+        self.dept=department
+        self.exp=experience
+        self.salary=salary
+
