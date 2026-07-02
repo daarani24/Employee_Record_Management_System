e1c3b6e1e45ddb2af4182d2a849e61e6592fc86a
daarani24 <daarani2007@gmail.com>
2026-07-02 23:19:27 +0530

    modified

--- main.py
+++ main.py
@@ -1,35 +1,42 @@
+from employee import Employee
+from file_handler import FileHandler
+from report import Report
 from employee_manager import EmployeeManager
 
-manager = EmployeeManager()
+manager=EmployeeManager()
 
 while True:
-    print("\n=============================")
-    print("  EMPLOYEE MANAGEMENT SYSTEM ")
-    print("=============================")
+    print("\nEMPLOYEE MANAGEMENT SYSTEM")
     print("1. Add Employee")
     print("2. View All Employees")
     print("3. Search Employee")
     print("4. Update Employee")
     print("5. Delete Employee")
-    print("6. Save Data to File")
-    print("7. Exit")
+    print("6. Highest Salary Employee")
+    print("7. Experienced Employees")
+    print("8. Save Data to File")
+    print("9. Exit")
 
-    choice = input("\nEnter your choice (1-7): ")
+    choice=input("\nEnter your choice (1-9): ")
 
-    if choice == "1":
+    if choice=="1":
         manager.add_employee()
-    elif choice == "2":
+    elif choice=="2":
         manager.view_employee()
-    elif choice == "3":
+    elif choice=="3":
         manager.search_employee()
-    elif choice == "4":
+    elif choice=="4":
         manager.update_employee()
-    elif choice == "5":
+    elif choice=="5":
         manager.delete_employee()
-    elif choice == "6":
+    elif choice=="6":
+        manager.highest_salary()
+    elif choice=="7":
+        manager.experienced_employee()
+    elif choice=="8":
         manager.save_to_file()
-    elif choice == "7":
+    elif choice=="9":
         print("\nThank you for using Employee Management System.")
         break
     else:
-        print("Invalid choice. Please enter a number between 1 and 7.")
+        print("Invalid choice. Please enter a number between 1 and 9.")
